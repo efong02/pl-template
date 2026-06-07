cc3c325172995bbeebe00a9ec14325b138e5d334
Eileen <efong02@gmail.com>
2026-06-07 19:28:04 -0400

    fixed division by 0 error

--- questions/horizontal-asymptote/server.py
+++ questions/horizontal-asymptote/server.py
@@ -10,14 +10,15 @@ def generate(data):
     d = random.randrange(1,6)
     e = random.randrange(1,6)
     f = random.randrange(1,6)
-    g = random.randrange(-2,2)
+    g = random.randrange(1,6)
+    h = random.choice([1, -1])
     
     #since degree for both is fixed at 2, only the leading coefficients matter for horizontal asymptote
     horizontalAsymptote = a/(e*g)
 
     x = sympy.symbols("x")
 
-    data["params"]["rationalPolynomial"] = sympy.latex((a * x**2 + b * x + c)/(d - e * x)(f - g * x))   
+    data["params"]["rationalPolynomial"] = sympy.latex((a * x**2 + b * x + c)/(d - e * x)(f - h * g * x))   
     data["correct_answers"]["ans"] = horizontalAsymptote
 
 
