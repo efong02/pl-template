68d1d1aded79090235b21608b7c7e22034f5abc5
Eileen <efong02@gmail.com>
2026-06-08 19:07:03 -0400

    changed wording for complete the square question

--- questions/complete-the-square/server.py
+++ questions/complete-the-square/server.py
@@ -1,8 +1,8 @@
 import random
 
 def generate(data):
-    # form is (x-a)^2 = b^2
     # displayed form is x^2 + 2ax = b^2-a^2
+    # this is so that the numbers are nice
     a = random.randrange(1,13)
     b = random.randrange(1,13)
 
@@ -15,7 +15,7 @@ def generate(data):
     data["params"]["rhs"] = rhs
     data["params"]["coeff"] = coeff
 
-    #user will enter smaller solution first, then larger solution.
+    # user will enter smaller solution first, then larger solution.
     r1 = min(a-b,a+b)
     r2 = max(a-b,a+b)
     data["correct_answers"]["r1"] = r1
